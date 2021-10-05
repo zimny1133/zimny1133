c68a447e1a1f4f133b8be9d6e0f60038f08ed2bf
DamianNowak <38436961+zimny1133@users.noreply.github.com>
2021-10-05 21:43:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 //code
 
-//adding new list element
+//adding list element
 
+function Puciu(){
+
+var myFinishedGames = document.getElementById('game-list');
+
+var newElementList = document.createElement('li')
+
+var fusion = myFinishedGames.appendChild(newElementList);
+
+var namingList = document.getElementsByTagName('li').item(9);
+
+namingList.innerHTML = ('Mario Odysey');
+};
+
+window.onload = Puciu();
+
+//adding list element
+
+
+
+
+    
+    
