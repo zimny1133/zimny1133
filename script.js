fd92aaca11e2d8d9ef2f2f30d69a674c47f96462
DamianNowak <38436961+zimny1133@users.noreply.github.com>
2021-10-31 20:38:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,27 @@ function displayVideo(){
     document.getElementsByClassName('video-button').style.display = 'none';
     
 };
+
+window.onload = function(){displayButtonTop()};
+
+function displayButtonTop(){
+    
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        
+        document.getElementById('totop').style.display = "block";
+    } else {
+        
+        document.getElementById('totop').style.display = "none";
+    }    
+};
+
+function toTop(){
+    
+    document.body.scrollTop = 0;
+};
+
+
+
+
+    
+    
