9a5f4ed4a040945daea160a418bb26d937125f57
DamianNowak <38436961+zimny1133@users.noreply.github.com>
2022-10-19 16:40:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,45 @@ const radioButtons = document.getElementsByTagName("input");
 
 const tableMilk = document.createElement("table");
 
+
+const milkCounter =  ()  => {
+
+  document.getElementById("table").appendChild(tableMilk);
+   const tableBody = document.getElementById("tablebody");
+   
+   const d = new Date();
+d.getHours();
+   
+   tablebody.innerHTML += `
+<tr>
+<td>${radioButtons[i].value}</td>
+<td>${d.getHours() + ":" + d.getMinutes()}</td>
+</tr>
+
+
+`;  
+
+};
+
+
+
+radioButtons.forEach(item => {
+                     
+                     
+             if (radioButtons.checked === true) {
+   
+      radioButtons[i].addEventListener("click", milkCounter);
+       
+   
+} else {
+
+console.log("nic nie wybrane")
+}        
+                     
+                     });
+
+
+
 tableMilk.innerHTML = `
 
 
@@ -52,40 +91,8 @@ tableMilk.innerHTML = `
 
 
 
-const milkCounter =  ()  => {
-
-  document.getElementById("table").appendChild(tableMilk);
-   const tableBody = document.getElementById("tablebody");
-   
-   const d = new Date();
-d.getHours();
-   
-   tablebody.innerHTML += `
-<tr>
-<td>${radioButtons[i].value}</td>
-<td>${d.getHours() + ":" + d.getMinutes()}</td>
-</tr>
 
 
-`;  
-
-};
-
-for (var i = 0; i < radioButtons.length; i++){
-
-    
-   
-   if (radioButtons.checked == true) {
-   
-      radioButtons[i].addEventListener("click", milkCounter);
-       
-   
-} else {
-
-console.log("nic nie wybrane")
-}
-   };
-  
 
 
 
